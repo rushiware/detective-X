89004abc378ec26bb31d51389d9f21617dda39ce
rushikeshware <rushikeshware263@gmail.com>
2021-08-12 18:30:51 +0530

    image and otherfiles

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <graphics.h>
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	int h=GetSystemMetrics(SM_CYSCREEN);
+	
+	int w=GetSystemMetrics(SM_CXSCREEN);
+	initwindow(w,h,"DETECTIVE X");
+	
+	readimagefile("images/homepage.jpeg",1,1,w,h);
+	
+	getch();
+	cleardevice();
+	
+	
+	
+	closegraph();	
+}
